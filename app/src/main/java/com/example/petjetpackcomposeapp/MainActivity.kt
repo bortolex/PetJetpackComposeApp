d0b63d811b2f5a9e710e3132f7ad327934cbe9ab
Oleksandr Bortnyk <oleksandr.bortnyk@walkme.com>
2025-03-18 14:08:44 +0200

    added first jetpackCompose component (Text component) - printing HelloWorld

--- app/src/main/java/com/example/petjetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/petjetpackcomposeapp/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
-
+            Text("Hello World",
+                fontSize =  35.sp
+                )
         }
     }
 }
