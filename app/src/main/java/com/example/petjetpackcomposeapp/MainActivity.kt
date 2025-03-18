7bc8fce7e9fd957a29c7a1697927c62a81de3a35
Oleksandr Bortnyk <oleksandr.bortnyk@walkme.com>
2025-03-18 14:13:05 +0200

    added background and color of text

--- app/src/main/java/com/example/petjetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/petjetpackcomposeapp/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,12 +23,14 @@ class MainActivity : ComponentActivity() {
             HelloWorld()
         }
     }
-    @Preview
+    @Preview (showBackground = true)
     @Composable
     fun HelloWorld() {
         Text(
             "Hello World",
-            fontSize = 35.sp
+            fontSize = 35.sp,
+            color = Color.Green,
+            modifier = Modifier.background(Color.Cyan)
         )
     }
 }
