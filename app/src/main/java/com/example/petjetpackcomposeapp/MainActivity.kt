e47d1b70bd0c0ee3153e812305139bd9b7375f09
Oleksandr Bortnyk <oleksandr.bortnyk@walkme.com>
2025-03-18 14:24:33 +0200

    added Row container

--- app/src/main/java/com/example/petjetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/petjetpackcomposeapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
     @Preview (showBackground = true)
     @Composable
     fun HelloWorld() {
-        Column {
+        Row {
             Text(
                 "Hello World",
                 fontSize = 35.sp,
