0402d2f8a2d9f6466332384aa5427dccf6be2330
Oleksandr Bortnyk <oleksandr.bortnyk@walkme.com>
2025-04-14 16:52:57 +0300

    added libs for navigation in compose; added first screen (HomeScreen) but without implemented states and viewModel (so this code is commented for now)

---
+++ app/src/main/java/com/example/petjetpackcomposeapp/AppNavHost.kt
@@ -0,0 +1,21 @@
+package com.example.petjetpackcomposeapp
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.petjetpackcomposeapp.screens.HomeScreen
+
+@Composable
+fun NavHost(navController: NavHostController) {
+    NavHost(navController, startDestination = "home") {
+        composable("home") { HomeScreen(navController) }
+//        composable("profile") { ProfileScreen(navController) }
+//        composable("settings") { SettingsScreen(navController) }
+
+//        composable("notifications") { NotificationsScreen(navController) }
+//        composable("details") { DetailsScreen(navController) }
+    }
+}
+
+
