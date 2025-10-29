61fb10dc6aab6fe11d8e36d36643f010967200bb
Oleksandr Bortnyk <oleksandr.bortnyk@walkme.com>
2025-10-30 00:16:28 +0200

    implementation of NavigationHost and EmptyRouter

--- navigation/src/main/java/com/example/navigation/NavigationHost.kt
+++ navigation/src/main/java/com/example/navigation/NavigationHost.kt
@@ -1,7 +1,14 @@
 package com.example.navigation
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
+import com.example.navigation.internal.EmptyRouter
+
+val LocalRouter = staticCompositionLocalOf<Router> { EmptyRouter }
 
 @Composable
 fun NavigationHost(
@@ -9,5 +16,15 @@ fun NavigationHost(
     modifier: Modifier = Modifier,
     routeMapper: @Composable (Route) -> Unit)
 {
+    val (router, navigationState) = navigation
+    BackHandler(enabled = !navigationState.isRoot) {
+        router.pop()
+    }
+    Box(modifier = modifier) {
+        CompositionLocalProvider {
+            LocalRouter provides router
+        }
+        routeMapper.invoke(navigationState.currentRoute)
+    }
 
 }
---
+++ navigation/src/main/java/com/example/navigation/internal/EmptyRouter.kt
@@ -0,0 +1,19 @@
+package com.example.navigation.internal
+
+import com.example.navigation.Route
+import com.example.navigation.Router
+
+internal object EmptyRouter:Router {
+    override fun launch(route: Route) {
+        TODO("Not yet implemented")
+    }
+
+    override fun pop() {
+        TODO("Not yet implemented")
+    }
+
+    override fun restart(route: Route) {
+        TODO("Not yet implemented")
+    }
+
+}
