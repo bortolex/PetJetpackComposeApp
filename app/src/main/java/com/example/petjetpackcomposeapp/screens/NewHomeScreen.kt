af66378fd441d4cae563e7ca44fb036f9fbc11c0
Oleksandr Bortnyk <oleksandr.bortnyk@walkme.com>
2026-01-30 12:12:09 +0200

    stylization of topAppBar accroding to figma Material 3 design - https://www.figma.com/design/uLTfxipGzfduuw8IHU5rPP/Untitled?node-id=1-987&m=dev

--- app/src/main/java/com/example/petjetpackcomposeapp/screens/NewHomeScreen.kt
+++ app/src/main/java/com/example/petjetpackcomposeapp/screens/NewHomeScreen.kt
@@ -1,18 +1,26 @@
 package com.example.petjetpackcomposeapp.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.petjetpackcomposeapp.R
+import com.example.petjetpackcomposeapp.ui.theme.TopAppBarSurfaceColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,9 +37,18 @@ fun NewHomeScreen(clickNavigation: () -> Unit) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaseTopAppBar() {
-    TopAppBar(title = { Text(text = "New Home Screen") }, navigationIcon = {
+    TopAppBar(modifier = Modifier.background(
+        color = TopAppBarSurfaceColor, shape = RoundedCornerShape(36.dp)
+    ), title = { Text(text = stringResource(R.string.title_top_app_bar)) }, navigationIcon = {
         Icon(
-            imageVector = Icons.Default.Face, contentDescription = null
+            imageVector = Icons.Default.Favorite, contentDescription = null
         )
-    })
+    },
+        // Make TopAppBar container transparent so Surface shows
+        // (we couldn't apply our own background color and shape without this piece of code)
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Transparent
+        )
+
+    )
 }
---
+++ app/src/main/java/com/example/petjetpackcomposeapp/ui/theme/Color.kt
@@ -0,0 +1,5 @@
+package com.example.petjetpackcomposeapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TopAppBarSurfaceColor = Color(0xFFF3EDF7)
