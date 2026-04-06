5388a42af241d1309dfcb7d7a4090c1ae8a17eaa
oleksandrbortnyk <alex.bortnik.it@gmail.com>
2026-04-06 16:20:01 +0300

    added shape for items in lazyColumn; handling click

--- app/src/main/java/com/example/petjetpackcomposeapp/ui/screens/ArticlesScreen.kt
+++ app/src/main/java/com/example/petjetpackcomposeapp/ui/screens/ArticlesScreen.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -38,15 +41,29 @@ fun ArticlesScreen() {
             }
         }
         is ArticlesUiState.Success -> {
-            ArticlesListView((state.value as ArticlesUiState.Success).data)
+            ArticlesListView((state.value as ArticlesUiState.Success).data){
+
+            }
         }
     }
 }
 
 @Composable
-fun ArticlesListView(data: List<Article>) {
+fun ArticleDetails(){
+
+}
+
+@Composable
+fun ArticlesListView(data: List<Article>,  onItemClick: (Article) -> Unit = {}) {
     LazyColumn(modifier = Modifier) {
         items(data) { item ->
+            Surface( modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .clickable { onItemClick(item) },
+                shape = RoundedCornerShape(16.dp),
+                color = Color.White,
+                shadowElevation = 6.dp) { }
             Column(modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)) {
                 Text(item.title, style = TextStyle(textDecoration = TextDecoration.Underline))
                 Spacer(Modifier.height(24.dp))
