2b60d98fc5aefe746f76e3c155d4863a3f5135d2
oleksandrbortnyk <alex.bortnik.it@gmail.com>
2026-05-01 17:19:06 +0300

    added counterScreen function and needed dependencies for writing unit tests in Compose

---
+++ app/src/main/java/com/example/petjetpackcomposeapp/ui/CounterScreen.kt
@@ -0,0 +1,31 @@
+package com.example.petjetpackcomposeapp.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CounterScreen() {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "$count", fontSize = 48.sp)
+        Button(onClick = { count++ }) {
+            Text("Increment")
+        }
+    }
+}
