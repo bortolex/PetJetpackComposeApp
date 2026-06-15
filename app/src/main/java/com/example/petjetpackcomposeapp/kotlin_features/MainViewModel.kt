ad6eee66e30016022278b13f03cb5d8c831483ea
oleksandrbortnyk <alex.bortnik.it@gmail.com>
2026-06-15 14:53:25 +0300

    working on Command pattern (Behavioral patterns)

--- app/src/main/java/com/example/petjetpackcomposeapp/kotlin_features/MainViewModel.kt
+++ app/src/main/java/com/example/petjetpackcomposeapp/kotlin_features/MainViewModel.kt
@@ -1,14 +1,21 @@
 package com.example.petjetpackcomposeapp.kotlin_features
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import java.math.BigDecimal
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(val savedStateHandle: SavedStateHandle) : ViewModel() {
+
+    val query: StateFlow<String> = savedStateHandle.getStateFlow("query", "")
 
     private val commands = listOf(
         LoadUserCommand(),
@@ -17,6 +24,8 @@ class MainViewModel @Inject constructor() : ViewModel() {
     )
 
     fun startWork() {
+        Channel
+
         viewModelScope.launch(CoroutineName("ParentCoroutine")) {
             commands.forEach { command ->
                 launch(command.coroutineName) {
